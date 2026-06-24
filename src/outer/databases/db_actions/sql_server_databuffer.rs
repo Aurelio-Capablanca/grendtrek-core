1fd89ae4dc7a451032fd2ba16f0e85f0b706f13f
Aurelio <aurelio.iempereur@gmail.com>
2026-06-23 21:09:12 -0600

    quickfix
    + problem with present join for types
    
    + adding geography for forced UDT cast to VARCHAR

--- src/outer/databases/db_actions/sql_server_databuffer.rs
+++ src/outer/databases/db_actions/sql_server_databuffer.rs
@@ -25,23 +25,25 @@ fn rows_to_canonnical(row: &Row) -> Result<Vec<CanonnicalColumns>, Box<dyn std::
     Ok(Vec::new())
 }
 
-fn query_builder(columns: &Vec<ColumnMembers>) -> String {    
-    columns.iter().map(|col| {        
-        let mut terms_query = String::new();        
-        if col.get_data_type().eq_ignore_ascii_case("hierarchyid") &&
-           col.get_data_type().eq_ignore_ascii_case("xml") {
-            terms_query.push_str(
-                &format!(
-                    "{:?}.toString() as {:?}",
-                    col.get_column_name(),
-                    col.get_column_name()
-                )
-                .to_string(),
-            );
-        }
-        terms_query
-    }).collect::<Vec<String>>()
-    .join(",")    
+fn query_builder(columns: &Vec<ColumnMembers>) -> String {
+    columns
+        .iter()
+        .map(|col| {
+            let col_name = col.get_column_name();
+            println!("Current Column: {:?}", col_name);
+            if col.get_data_type().eq_ignore_ascii_case("hierarchyid") {
+                format!("CAST([{}] as VARCHAR) as [{}]", col_name, col_name)
+            } else if col.get_data_type().eq_ignore_ascii_case("xml")
+                || col.get_data_type().eq_ignore_ascii_case("geography")
+                || col.get_column_name().eq_ignore_ascii_case("other")
+            {
+                format!("CAST([{}] as NVARCHAR(max)) as [{}]", col_name, col_name)
+            } else {
+                format!("[{}]", col_name)
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" , ")
 }
 
 pub async fn get_rows_from_tables(
@@ -51,6 +53,7 @@ pub async fn get_rows_from_tables(
     for metadata in tables_metadata {
         let table_key: &(String, String) = metadata.0;
         let table_metadata: &TableMetadata = metadata.1;
+        println!("Reference columns : {:?}", table_metadata.get_cols_as_ref());
         let empty_otherwise = &SQLConstraints::PRIMARYKEY(IdentitySpecification::empty_struct());
         let pk_identifier = table_metadata
             .get_constrs_as_ref()
@@ -62,7 +65,7 @@ pub async fn get_rows_from_tables(
             .unwrap_or(empty_otherwise);
         let columns_query = query_builder(table_metadata.get_cols_as_ref());
         let query_build = format!(
-            "SELECT {:?} FROM {:?}.{:?} ORDER BY {:?}  OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;",
+            "SELECT {} FROM [{}].[{}] ORDER BY [{}]  OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;",
             columns_query,
             table_key.1,
             table_key.0,
